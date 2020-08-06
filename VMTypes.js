d16affc1c1f2f9dbe005ccb9757e1cae658ef6bb
Jochem Bek <Jochem_bek@hotmail.com>
2020-08-06 15:44:19 +0200

    removed

--- VMTypes.js
+++ VMTypes.js
@@ -21,8 +21,7 @@ function getDetails() {
         url: `https://management.azure.com/subscriptions/78240be1-b35a-4b11-a390-c408e3100d28/providers/Microsoft.Compute/skus?api-version=2019-04-01$filter=location%20eq%20'ukwest'`,            
         type: 'GET',
         beforeSend: function(xhr) { 
-          xhr.setRequestHeader("Authorization", "Bearer eyJ0eXAiOiJKV1QiLCJhbGciOiJSUzI1NiIsIng1dCI6Imh1Tjk1SXZQZmVocTM0R3pCRFoxR1hHaXJuTSIsImtpZCI6Imh1Tjk1SXZQZmVocTM0R3pCRFoxR1hHaXJuTSJ9.eyJhdWQiOiJodHRwczovL21hbmFnZW1lbnQuY29yZS53aW5kb3dzLm5ldC8iLCJpc3MiOiJodHRwczovL3N0cy53aW5kb3dzLm5ldC9kYzkxYWNhYS01NDUxLTQzNTktOGJiNS1iMmIyMTk4NzdlYmYvIiwiaWF0IjoxNTk2NjQyMDYyLCJuYmYiOjE1OTY2NDIwNjIsImV4cCI6MTU5NjY0NTk2MiwiYWlvIjoiRTJCZ1lEQm83YzJlZnRmdmtZcEd2bU9kN0I5TkFBPT0iLCJhcHBpZCI6IjIyNTEwZDYyLWM3N2ItNDEyZC1iNzE3LWJkNjhkNjkxNjE5MiIsImFwcGlkYWNyIjoiMSIsImlkcCI6Imh0dHBzOi8vc3RzLndpbmRvd3MubmV0L2RjOTFhY2FhLTU0NTEtNDM1OS04YmI1LWIyYjIxOTg3N2ViZi8iLCJvaWQiOiI1OWQyZTQ3My1kZDU5LTQ0N2QtOGRjNS02OTdlMWZiNTY4MzYiLCJzdWIiOiI1OWQyZTQ3My1kZDU5LTQ0N2QtOGRjNS02OTdlMWZiNTY4MzYiLCJ0aWQiOiJkYzkxYWNhYS01NDUxLTQzNTktOGJiNS1iMmIyMTk4NzdlYmYiLCJ1dGkiOiJTaXdqOXBtbWdVbUJSYnhwNWhrX0FBIiwidmVyIjoiMS4wIiwieG1zX3RjZHQiOjE1OTY2MTE2OTJ9.J2HbcvGQTY_duxCl7bj7A4igjJDv2qyXcr6o46RtXqlNubliSbkQyM2odkW3ZKR1ACOSc4mZGEVGRwHZoVuD0NX7T5l6czK7sqwPGMjhvTBg0OWjMjiiZJFm-Y5bAxKVrzA3rVXOe2DxAbrI-yOiqTc1hzywopjUfkLv2k7BxwmEZMYyNLUMG5aqUgBSJx2ghjLU2kLqFUdHBDu-IsN0YRirDNeE6Q6NUPBIW_WmnVYbvtU8Rkho0AHPTOGUIHMdNnX-haNOGPYkv3lVdsCa9B5_4RLM_2-xUEEuT17tbcpcb7q8HNMtpvNUqTN8OBDJv8q93ei57YBIES6sMYTDxw"); 
-        },
+          xhr.setRequestHeader("Authorization",)},
         success: function (data) {
             console.log(data.value);
         },
